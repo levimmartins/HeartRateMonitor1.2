05cefe8770952e1d63d3e5924996b47d4970a4f7
Levi Martins <levimmartins@gmail.com>
2017-12-11 10:52:23 -0300

    ...

---
+++ conquistas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Coquistas</title>
+    <link rel="stylesheet" href="css/home.css"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+
+    <body class="body_atualizado">
+        
+    <?php include 'navbar.php'?>    
+        
+        <div class="container">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <img src="img/1.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/2.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/3.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/4.png" width="90%"/>
+                    </div>   
+                </div>
+                 <div class="row">
+                    <div class="col">
+                        <img src="img/5.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/6.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/7.png" width="90%"/>
+                    </div>   
+                    <div class="col">
+                        <img src="img/8.png" width="90%"/>
+                    </div>   
+                </div>
+            </div>
+        </div>    
+        
+        
+        
+        
+    </body>
+</html>
